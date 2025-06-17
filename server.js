3f749f8a5274b67a0e8dd0bc646d774a76ecf38e
lloprestii <161731269+lloprestii@users.noreply.github.com>
2025-06-17 02:46:06 -0300

    Update server.js

--- server.js
+++ server.js
@@ -61,5 +61,5 @@ app.post('/api/cotizar', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
